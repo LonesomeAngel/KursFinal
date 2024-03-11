b1eb86320fdbae75ef86cd4a5bb7a51faa781750
Mikael <nasstyangel@yandex.ru>
2024-03-11 15:31:52 +0300

    написана программа формирующая массив из строк длинной 3 и меньше символов

--- Program.cs
+++ Program.cs
@@ -1,2 +1,52 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Написать программу, которая из имеющегося массива строк 
+// формирует новый массив из строк, длина которых меньше, 
+//либо равна 3 символам. Первоначальный массив можно ввести 
+// с клавиатуры, либо задать на старте выполнения алгоритма. 
+// При решении не рекомендуется пользоваться коллекциями, 
+// лучше обойтись исключительно массивами.
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод массива строк с клавиатуры
+        Console.WriteLine("Введите элементы массива (разделяйте каждый элемент нажатием клавиши Enter). Для завершения введите пустую строку:");
+        string[] inputArray = ReadInputArray();
+
+        // Формирование нового массива из строк, длина которых <= 3 символам
+        string[] resultArray = FilterShortStrings(inputArray);
+
+        // Вывод результата
+        Console.WriteLine("\nРезультат:");
+        foreach (string str in resultArray)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    // Метод для ввода массива строк с клавиатуры
+    static string[] ReadInputArray()
+    {
+        var inputList = new System.Collections.Generic.List<string>();
+        string input;
+        while (!string.IsNullOrWhiteSpace(input = Console.ReadLine()))
+        {
+            inputList.Add(input);
+        }
+        return inputList.ToArray();
+    }
+
+    // Метод для формирования нового массива из строк, длина которых <= 3 символам
+    static string[] FilterShortStrings(string[] inputArray)
+    {
+        var resultList = new System.Collections.Generic.List<string>();
+        foreach (string str in inputArray)
+        {
+            if (str.Length <= 3)
+            {
+                resultList.Add(str);
+            }
+        }
+        return resultList.ToArray();
+    }
+}
